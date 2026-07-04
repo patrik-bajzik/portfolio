5b8e4e4b7aef2a0d8b2e1b24d78bd5aeff6d4954
Patrik Bajzík <bajzik.patrik6@gmail.com>
2026-07-04 20:34:54 +0200

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Technologies} from './components/Technologies';
 import {Projects} from './components/Projects';
 import {Contact} from './components/Contact';
 import {Footer} from './components/Footer';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Contact/>
       </main>
       <Footer/>
+      <Analytics />
     </div>
   );
 }
